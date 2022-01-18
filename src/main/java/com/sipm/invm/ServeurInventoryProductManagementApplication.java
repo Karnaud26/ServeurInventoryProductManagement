fe436c2394a14c337117bc7f088294feafadd4ae
Arnaud Kemgouo <tcheukem26@gmail.com>
2022-01-18 14:59:32 +0100

    Projet3serveur:Initialisation de la partie back end

---
+++ src/main/java/com/sipm/invm/ServeurInventoryProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sipm.invm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurInventoryProductManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurInventoryProductManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sipm/invm/ServeurInventoryProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipm.invm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurInventoryProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
